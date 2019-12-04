e32e7295b4c830343e8e22fcda0abaeab7ff6582
npalla16 <neelimapalla@gmail.com>
2019-12-04 13:39:27 +0000

    comments in main class

--- FizzBuzzTask/src/main/java/co/uk/myprojects/FizzBuzzTask.java
+++ FizzBuzzTask/src/main/java/co/uk/myprojects/FizzBuzzTask.java
@@ -4,22 +4,10 @@ import java.util.stream.IntStream;
 
 public class FizzBuzzTask {
 
-	//Print FIZZ instead of number,if the number is multiple of 3 or it has 3 in it.
-	//Print BUZZ instead of number,if the number is multiple of 5 or it has 5 in it.
-	//Print FIZZBUZZ instead of number,if the number satisfies both criteria.
+	/** Print FIZZ instead of number,if the number is multiple of 3 or it has 3 in it.
+	Print BUZZ instead of number,if the number is multiple of 5 or it has 5 in it.
+	Print FIZZBUZZ instead of number,if the number satisfies both criteria. */
 	
-	/* public static void processNumber() {
-		 for(int i=1;i<=100;i++)
-		 {
-			 if(i%3==0 && i%5==0)
-			   System.out.println("FIZZBUZZ");
-			 else if (i%3==0)System.out.println("FIZZ");
-			 else if (i%5 ==0)System.out.println("BUZZ"); 
-			 else System.out.println(i);
-		 }
-		 
-	 }
-	 */
 	
 	private java.util.function.IntPredicate divBy(int number){
 		return i-> i % number == 0;
